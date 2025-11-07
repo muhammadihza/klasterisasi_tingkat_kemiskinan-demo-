4e4270fd80edf5867da19382790840a41bf94890
muhammadihza <101081532+muhammadihza@users.noreply.github.com>
2025-11-07 22:49:40 +0700

    Refactor labeling_result to remove input prompts Test
    Removed user input for cluster labels and related logic.

--- atribut/labeling_hasil.py
+++ atribut/labeling_hasil.py
@@ -5,16 +5,5 @@ def labeling_result(n, df):
     
     labels = []
 
-    for i in range(n):
-        huruf = chr(65 + i)  # Mengonversi indeks ke huruf (A, B, C, ...)
-        label = st.text_input(f"Masukkan label untuk klaster {i+1} (misalkan {huruf}):")
-        labels.append(label)
-
-    df['Cluster'] = df['Cluster'].astype('object')
     
-    conditions = [(df['Cluster'] == i) for i in range(n)]
-    choices = labels[:n]
-    
-    df['Cluster'] = np.select(conditions, choices)
-
     return df
